866a3fe783c5ac65e75e42d18f32daeedfcc9651
Vooperino <voop@voop.lv>
2023-12-10 22:34:46 +0200

    Updated Core build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.0"
     id("io.papermc.paperweight.userdev") version "1.5.8" apply false
+    id("tythan-global-conventions")
 }
 
 repositories {
@@ -9,7 +10,9 @@ repositories {
 
 allprojects {
     apply(plugin = "com.github.johnrengelman.shadow")
+    apply(plugin = "tythan-global-conventions")
     apply(plugin = "java")
+
     group = "com.github.archemedes.tythan"
     version = "1.0.0-SNAPSHOT"
 }
